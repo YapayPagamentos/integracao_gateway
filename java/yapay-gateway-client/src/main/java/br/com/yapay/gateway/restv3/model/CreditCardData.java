ea680dec9355050bbd215112435103ef7aac48b6
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-09-28 14:02:25 -0300

    credit and debit cards classes created

--- java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/restv3/model/CreditCardData.java
+++ java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/restv3/model/CreditCardData.java
@@ -1,5 +1,84 @@
 package br.com.yapay.gateway.restv3.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CreditCardData {
+
+	@SerializedName("nomePortador")
+	private String cardHolderName;
+
+	@SerializedName("numeroCartao")
+	private String cardNumber;
+
+	@SerializedName("codigoSeguranca")
+	private String cvv;
+
+	@SerializedName("dataValidade")
+	private String expirationDate;
+
+	@SerializedName("codigoFormaPagamento")
+	private Integer paymentCode;
+
+	@SerializedName("parcelas")
+	private Integer installments;
+
+	@SerializedName("valor")
+	private Long value;
+
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+
+	public String getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(String expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public Integer getPaymentCode() {
+		return paymentCode;
+	}
+
+	public void setPaymentCode(Integer paymentCode) {
+		this.paymentCode = paymentCode;
+	}
+
+	public Integer getInstallments() {
+		return installments;
+	}
+
+	public void setInstallments(Integer installments) {
+		this.installments = installments;
+	}
+
+	public Long getValue() {
+		return value;
+	}
+
+	public void setValue(Long value) {
+		this.value = value;
+	}
 
 }
--- java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/restv3/model/DebitCardData.java
+++ java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/restv3/model/DebitCardData.java
@@ -1,5 +1,62 @@
 package br.com.yapay.gateway.restv3.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class DebitCardData {
+
+	@SerializedName("agencia")
+	private String agency;
+
+	@SerializedName("agenciaDigito")
+	private String agencyDigit;
+
+	@SerializedName("conta")
+	private String accountNumber;
+
+	@SerializedName("contaDigito")
+	private String accountNumberDigit;
+
+	@SerializedName("tipoConta")
+	private String accountType;
+
+	public String getAgency() {
+		return agency;
+	}
+
+	public void setAgency(String agency) {
+		this.agency = agency;
+	}
+
+	public String getAgencyDigit() {
+		return agencyDigit;
+	}
+
+	public void setAgencyDigit(String agencyDigit) {
+		this.agencyDigit = agencyDigit;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAccountNumberDigit() {
+		return accountNumberDigit;
+	}
+
+	public void setAccountNumberDigit(String accountNumberDigit) {
+		this.accountNumberDigit = accountNumberDigit;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
 
 }
