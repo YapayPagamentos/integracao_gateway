83389395748bf91b3bf7ffb39f54c5edf6c9bd98
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-01 11:36:39 -0300

    include build method to transaction builder and include extra and items list to a transaction

--- java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/builder/TransactionBuilder.java
+++ java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/builder/TransactionBuilder.java
@@ -1,6 +1,11 @@
 package br.com.yapay.gateway.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.yapay.gateway.restv3.model.CreditCardData;
+import br.com.yapay.gateway.restv3.model.ExtraField;
+import br.com.yapay.gateway.restv3.model.ItemData;
 import br.com.yapay.gateway.restv3.model.Transaction;
 import br.com.yapay.gateway.restv3.model.TransactionData;
 
@@ -16,17 +21,56 @@ public class TransactionBuilder {
 		transaction.getTransactionData().setTransactionNumber(transactionNumber);
 		transaction.getTransactionData().setValue(value);
 	}
-	
+
 	public static void withInstallments(Integer installments) {
 		transaction.getTransactionData().setInstallments(installments);
 	}
-	
-	public static void withSingleCreditCard(String cardHolderName, String cardNumber, String cvv, String expirationDate) {
+
+	public static void withSingleCreditCard(String cardHolderName, String cardNumber, String cvv,
+			String expirationDate) {
 		transaction.setCreditCard(new CreditCardData());
 		transaction.getCreditCard().setCardHolderName(cardHolderName);
 		transaction.getCreditCard().setCardNumber(cardNumber);
 		transaction.getCreditCard().setCvv(cvv);
 		transaction.getCreditCard().setExpirationDate(expirationDate);
+	}
+
+	public static void withItems(List<ItemData> listOfItems) {
+		List<ItemData> transactionItemList = new ArrayList<>();
+
+		if (listOfItems != null) {
+			for (ItemData item : listOfItems) {
+				ItemData transactionItem = new ItemData();
+				transactionItem.setCategoryName(item.getCategoryName());
+				transactionItem.setProductAmount(item.getProductAmount());
+				transactionItem.setProductCategory(item.getProductCategory());
+				transactionItem.setProductCode(item.getProductCode());
+				transactionItem.setProductName(item.getProductName());
+				transactionItem.setProductUnitaryValue(item.getProductUnitaryValue());
+				transactionItemList.add(transactionItem);
+			}
+		}
+
+		transaction.setItems(transactionItemList);
+	}
+
+	public static void withExtraFields(List<ExtraField> listOfExtraFields) {
+		List<ExtraField> transactionExtraFieldsList = new ArrayList<>();
+
+		if (listOfExtraFields != null) {
+			for (ExtraField extraField : listOfExtraFields) {
+				ExtraField transactionExtraField = new ExtraField();
+				transactionExtraField.setKey(extraField.getKey());
+				transactionExtraField.setValue(transactionExtraField.getValue());
+				transactionExtraFieldsList.add(transactionExtraField);
+			}
+		}
+
+		transaction.setExtraFields(transactionExtraFieldsList);
+	}
+	
+	public Transaction build() {
+		return transaction;
 	}
 
 }
