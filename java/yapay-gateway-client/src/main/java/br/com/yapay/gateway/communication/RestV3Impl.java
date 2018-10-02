3ed81cdc47c9c795718a9aca0af97d1074a1593e
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-02 10:50:47 -0300

    setting default basic authorization for all restv3 communication

--- java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/communication/RestV3Impl.java
+++ java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/communication/RestV3Impl.java
@@ -1,5 +1,12 @@
 package br.com.yapay.gateway.communication;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import br.com.yapay.gateway.model.Credential;
 import br.com.yapay.gateway.model.OneClickRegisterData;
 import br.com.yapay.gateway.model.RecurringPayment;
@@ -9,68 +16,94 @@ public class RestV3Impl implements RestV3 {
 
 	@Override
 	public String transactionAuthorize(Credential credential, Transaction transaction) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String transactionConsult(Credential credential, String storeCode, Long transactionNumber) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
+		
 		return null;
 	}
 
 	@Override
 	public String transactionCapture(Credential credential, String storeCode, Long transactionNumber, Long value) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String transactionCancel(Credential credential, String storeCode, Long transactionNumber, Long value) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String oneClickRegister(Credential credential, OneClickRegisterData registerData) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String oneClickConsult(Credential credential, String token) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String oneClickRegisterUpdate(Credential credential, OneClickRegisterData registerData) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String oneClickAuthorize(Credential credential, String token, Transaction transaction) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String recurringPaymentRegister(Credential credential, RecurringPayment recurringPayment) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String recurringPaymentConsult(Credential credential, String storeCode, Long recurringPaymentNumber) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
 	}
 
 	@Override
 	public String recurringPaymentCancel(Credential credential, String storeCode, Long recurringPaymentNumber) {
-		// TODO Auto-generated method stub
+		HttpClient client = this.httpClientBuilder(credential);
+		
 		return null;
+	}
+
+	private HttpClient httpClientBuilder(Credential gatewayCredential) {
+		HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(credentialSetup(gatewayCredential))
+				.build();
+		return client;
+	}
+
+	private CredentialsProvider credentialSetup(Credential gatewayCredential) {
+		CredentialsProvider credentialProviders = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(gatewayCredential.getUser(),
+				gatewayCredential.getPassword());
+		credentialProviders.setCredentials(AuthScope.ANY, credentials);
+		return credentialProviders;
 	}
 
 }
