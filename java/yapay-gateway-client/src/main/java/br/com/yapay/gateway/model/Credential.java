be2998a5dd3dd4cfb0a52f17d8e1a0d695bd5fe5
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-02 10:09:06 -0300

    setting up credential class attrs

--- java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/model/Credential.java
+++ java/yapay-gateway-client/src/main/java/br/com/yapay/gateway/model/Credential.java
@@ -2,4 +2,32 @@ package br.com.yapay.gateway.model;
 
 public class Credential {
 
+	private String user;
+	private String password;
+	private String storeCode;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStoreCode() {
+		return storeCode;
+	}
+
+	public void setStoreCode(String storeCode) {
+		this.storeCode = storeCode;
+	}
+
 }
