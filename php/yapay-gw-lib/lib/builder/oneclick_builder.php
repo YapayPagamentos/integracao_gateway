5d402e63743ea8ee2b72624bac6936031dd03921
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-09 15:07:39 -0300

    oneclick builder implementation

--- php/yapay-gw-lib/lib/builder/oneclick_builder.php
+++ php/yapay-gw-lib/lib/builder/oneclick_builder.php
@@ -1,17 +1,25 @@
 <?php 
 	
+	require_once "models/oneclick_register_data.php";
+
 	class OneclickBuilder {
+
+		private $oneClickRegisterData;
 		
 		public function registerNewOneClick($storeCode, $paymentCode){
-			
+			$oneClickRegisterData = new OneclickRegisterData();
+			$oneClickRegisterData->storeCode = $storeCode;
+			$oneClickRegisterData->paymentCode = $paymentCode;
 		}
 
 		public function withCreditCard($cardHolderName, $cardNumber, $expirationDate){
-			
+			$oneClickRegisterData->cardHolderName = $cardHolderName;
+			$oneClickRegisterData->cardNumber = $cardNumber;
+			$oneClickRegisterData->expirationDate = $expirationDate;
 		}
 
 		public function forEmail($buyerEmail){
-			
+			$oneClickRegisterData->buyerEmail = $buyerEmail;
 		}
 
 	}
