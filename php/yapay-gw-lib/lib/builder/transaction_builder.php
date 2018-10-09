768c33c120cd28b6eba074f163edf07f5835b9f5
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-09 14:46:47 -0300

    starting with transaction number

--- php/yapay-gw-lib/lib/builder/transaction_builder.php
+++ php/yapay-gw-lib/lib/builder/transaction_builder.php
@@ -1,17 +1,33 @@
 <?php 
+
+	require_once "models/transaction.php";
+	require_once "models/transaction_data.php";
+	require_once "models/transaction_card_data.php";
 	
 	class TransactionBuilder {
 		
+		private $transaction;
+
 		public function newTransaction($storeCode, $paymentCode, $transactionNumber, $value){
-			
+			$transaction = new Transaction();
+			$transaction->storeCode = $storeCode;
+			$transaction->paymentCode = $paymentCode;
+			$transaction->transactionData = new TransactionData();
+			$transaction->transactionData->transactionNumber = $transactionNumber;
+			$transaction->transactionData->value = $value;
+			$transaction->transactionData->installments = 1;
 		}
 
 		public function withInstallments($installments){
-			
+			$transaction->transactionData->installments = $installments;
 		}
 
 		public function withSingleCreditCard($cardHolderName, $cardNumber, $cvv, $expirationDate){
-			
+			$transaction->transactionCardData = new TransactionCardData();
+			$transaction->transactionCardData->cardHolderName = $cardHolderName;
+			$transaction->transactionCardData->cardNumber = $cardNumber;
+			$transaction->transactionCardData->cvv = $cvv;
+			$transaction->transactionCardData->expirationDate = $expirationDate;
 		}
 
 		public function withItems($arrayOfTransactionItem){
