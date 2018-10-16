16f93ed42cc623cbe1bd7f8db4022f3cdcf77e25
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-16 14:45:04 -0300

    starting design of transaction json representation builder

--- php/yapay-gw-lib/lib/json_builder/transaction_json_builder.php
+++ php/yapay-gw-lib/lib/json_builder/transaction_json_builder.php
@@ -1,7 +1,25 @@
 <?php 
 
+	require_once "json_representation/transaction_json.php";
+	require_once "json_representation/transaction_data_json.php";
+	require_once "json_representation/transaction_card_data_json.php";
+
 	class TransactionJsonBuilder{
 		
+
+		public function newTransaction($transaction){
+			$jsonTransaction = new TransactionJson();
+			$jsonTransaction->transacao = $this->newTransactionData($transaction->transactionData);
+			$jsonTransaction->dadosCartao = $this->newTransactionCardData($transaction->transactionCardData);
+		}
+
+		public function newTransactionData($transactionData){
+			$jsonTransactionData = new TransactionDataJson();
+		}
+
+		public function newTransactionCardData($transactionCardData){
+			$jsonTransactionCardData = new TransactionCardData();
+		}
 	}
 
  ?>
