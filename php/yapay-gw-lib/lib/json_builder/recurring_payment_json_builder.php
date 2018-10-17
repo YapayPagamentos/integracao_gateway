aee66b6b9f0af76a101e06d7ce6be853bad90033
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-17 09:20:36 -0300

    starting with recurring payment json builder

--- php/yapay-gw-lib/lib/json_builder/recurring_payment_json_builder.php
+++ php/yapay-gw-lib/lib/json_builder/recurring_payment_json_builder.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
+
+	require_once "json_representation/recurring_payment_json.php";
+	require_once "json_representation/recurring_payment_data_json.php";
 
 	class RecurringPaymentJsonBuilder{
 		
+		public function newRecurringPayment($recurringPayment){
+			$jsonRecurringPayment = new RecurringPaymentJson($recurringPayment);
+			$jsonRecurringPayment->recorrencia = $this->newRecurringPaymentData($recurringPayment->recurringPaymentData);
+
+			return $jsonRecurringPayment;
+		}
+
+		public function newRecurringPaymentData($recurringPaymentData){
+			$jsonRecurringPaymentData = new RecurringPaymentJson($recurringPaymentData);
+
+			return $jsonRecurringPaymentData;
+		}
 	}
 
  ?>
