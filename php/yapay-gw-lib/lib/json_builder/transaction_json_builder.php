b6cf3061818c169ba90907f26ae5ab699f414121
Ivan Simionato <ivan.simionato@superpay.com.br>
2018-10-18 10:47:17 -0300

    include creation of extra fields, debit card data and items json representation in builder

--- php/yapay-gw-lib/lib/json_builder/transaction_json_builder.php
+++ php/yapay-gw-lib/lib/json_builder/transaction_json_builder.php
@@ -3,6 +3,9 @@
 	require_once "json_representation/transaction_json.php";
 	require_once "json_representation/transaction_data_json.php";
 	require_once "json_representation/transaction_card_data_json.php";
+	require_once "json_representation/transaction_debit_data_json.php";
+	require_once "json_representation/transaction_item_data_json.php";
+	require_once "json_representation/transaction_extra_field_json.php";
 
 	class TransactionJsonBuilder{
 		
@@ -11,6 +14,9 @@
 			$jsonTransaction = new TransactionJson();
 			$jsonTransaction->transacao = $this->newTransactionData($transaction->transactionData);
 			$jsonTransaction->dadosCartao = $this->newTransactionCardData($transaction->transactionCardData);
+			$jsonTransaction->dadosDebito = $this->newTransactionDebitData($transaction->transactionDebitData);
+			$jsonTransaction->itensDoPedido = $this->newTransactionItemData($transaction->transactionItemData);
+			$jsonTransaction->camposExtras = $this->newTransactionExtraFields($transaction->transactionExtraFields);
 
 			return $jsonTransaction;
 		}
@@ -22,10 +28,40 @@
 		}
 
 		public function newTransactionCardData($transactionCardData){
-			$jsonTransactionCardData = new TransactionCardData();
+			$jsonTransactionCardData = new TransactionCardDataJson($transactionCardData);
 
 			return $jsonTransactionCardData;
 		}
+
+		public function newTransactionDebitData($transactionDebitData){
+			$jsonTransactionDebitData = new TransactionDebitDataJson($transactionDebitData);
+
+			return $jsonTransactionDebitData;
+		}
+
+		public function newTransactionItemData($transactionItemData){
+			$jsonArrayOfItems = array();
+
+			foreach ($transactionItemData as $originalItem) {
+				$jsonRepresentationOfItem = new TransactionItemDataJson($originalItem);
+				array_push($jsonArrayOfItems, $jsonRepresentationOfItem);
+			}
+
+			return $jsonArrayOfItems;
+		}
+
+		public function newTransactionExtraFields($transactionExtraFields){
+			$jsonArrayOfExtraFields = array();
+
+			foreach ($transactionExtraFields as $originalExtraField) {
+				$jsonRepresentationOfExtraField = new TransactionExtraFieldJson($originalExtraField);
+				array_push($jsonArrayOfExtraFields, $jsonRepresentationOfExtraField);
+			}
+
+			return $jsonArrayOfExtraFields;
+		}
+
+
 	}
 
  ?>
