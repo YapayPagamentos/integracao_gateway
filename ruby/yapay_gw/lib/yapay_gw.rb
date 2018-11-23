7c30c38361cb4e053a1c5633855b3ce0fc088eab
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-11-23 11:46:15 -0200

    require class in gem file

--- ruby/yapay_gw/lib/yapay_gw.rb
+++ ruby/yapay_gw/lib/yapay_gw.rb
@@ -1,4 +1,8 @@
 require "yapay_gw/version"
+require "yapay_gw/communication/rest_v3"
+require "yapay_gw/builder/oneclick_builder"
+require "yapay_gw/builder/transaction_builder"
+require "yapay_gw/builder/recurring_payment_builder"
 
 module YapayGw
   
