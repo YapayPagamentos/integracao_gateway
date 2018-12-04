74096b146a97cf3c0d227818b065655f956b222d
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-04 17:28:28 -0200

    transaction and transactiondata properties .net

--- net/YapayGatewayLib/model/Transaction.cs
+++ net/YapayGatewayLib/model/Transaction.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 namespace YapayGatewayLib.Model
 {
     public class Transaction
     {
+        string storeCode{get; set;}
+        int paymentCode{get; set;}
+        TransactionData transactionData{get; set;}
+        CheckoutData checkout{get; set;}
+        CreditCardData creditCard{get; set;}
+        List<CreditCardData> multipleCard{get; set;}
+        DebitCardData debitCard{get; set;}
+        List<ItemData> items{get; set;}
+        ChargingData charging{get; set;}
+        DeliveryData delivery{get; set;}
+        AirlineData airline{get; set;}
+        List<ExtraField> extraFields{get; set;}
     }
 }
--- net/YapayGatewayLib/model/TransactionData.cs
+++ net/YapayGatewayLib/model/TransactionData.cs
@@ -3,6 +3,28 @@ using System;
 namespace YapayGatewayLib.Model
 {
     public class TransactionData
-    {
+    { 
+	 long transactionNumber{get; set;}
+	 long value{get; set;}
+	 long discountValue{get; set;}
+	 long boardingFee{get; set;}
+	 int installments{get; set;}
+	 string installmentType{get; set;}
+	 string hintUrl{get; set;}
+	 string resultUrl{get; set;}
+	 string unpaidRedirectUrl{get; set;}
+	 int idiom{get; set;}
+	 string ip{get; set;}
+	 string browser{get; set;}
+	 int transactionOrigin{get; set;}
+	 string freeFieldOne{get; set;}
+	 string freeFieldTwo{get; set;}
+	 string freeFieldThree{get; set;}
+	 string freeFieldFour{get; set;}
+	 string freeFieldFive{get; set;}
+	 string billDueDate{get; set;}
+	 string country{get; set;}
+	 string currency{get; set;}
+     bool recurrentPayment{get; set;}
     }
 }
