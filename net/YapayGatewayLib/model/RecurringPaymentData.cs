935bacc5a3f06fc6d1ec029645fd5770878fb449
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-04 17:51:45 -0200

    recurring payment data, delivery data, multiple bill data properties

--- net/YapayGatewayLib/model/RecurringPaymentData.cs
+++ net/YapayGatewayLib/model/RecurringPaymentData.cs
@@ -4,5 +4,24 @@ namespace YapayGatewayLib.Model
 {
     public class RecurringPaymentData
     {
+	 long paymentCode{get;set;}
+	 long recurringPaymentNumber{get;set;}
+	 long value{get;set;}
+	 int modality{get;set;}
+	 int frequency{get;set;}
+	 string notificationUrl{get;set;}
+	 bool processImmediately{get;set;}
+	 int billingAmount{get;set;}
+	 string billingFirstDate{get;set;}
+	 string billDueDate{get;set;}
+	 string freeFieldOne{get;set;}
+	 string freeFieldTwo{get;set;}
+	 string freeFieldThree{get;set;}
+	 string freeFieldFour{get;set;}
+	 string freeFieldFive{get;set;}
+	 CreditCardData creditCard{get;set;}
+	 DebitCardData debitCard{get;set;}
+	 RecurringPaymentChargingData chargingData{get;set;}
+	 RecurringPaymentDeliveryData deliveryData{get;set;}
     }
 }
--- net/YapayGatewayLib/model/TransactionCheckoutMultipleBillData.cs
+++ net/YapayGatewayLib/model/TransactionCheckoutMultipleBillData.cs
@@ -4,5 +4,7 @@ namespace YapayGatewayLib.Model
 {
     public class TransactionCheckoutMultipleBillData
     {
+        long value{get;set;}
+        string dueDate{get;set;}
     }
 }
