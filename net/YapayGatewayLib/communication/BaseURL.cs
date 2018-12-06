e6eeccaa7e531cd92fa41a8b194c28f794dc4963
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-06 17:27:11 -0200

    include baseurl class in .net

---
+++ net/YapayGatewayLib/communication/BaseURL.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace YapayGatewayLib.Communication
+{
+    class BaseURL
+    {
+        public string sandbox = "https://sandbox.gateway.yapay.com.br/checkout";
+        public string production = "https://gateway.yapay.com.br/checkout";
+    }
+}
