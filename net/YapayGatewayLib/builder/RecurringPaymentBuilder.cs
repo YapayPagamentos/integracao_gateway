e633d657a6be9be5774751f6cca82935d9ac7a39
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-06 13:00:27 -0200

    complete implementation of recurringpayment builder .net

--- net/YapayGatewayLib/builder/RecurringPaymentBuilder.cs
+++ net/YapayGatewayLib/builder/RecurringPaymentBuilder.cs
@@ -82,11 +82,56 @@ namespace YapayGatewayLib.Builder
 
         public static void WithDelivery(RecurringPaymentDeliveryData deliveryData)
         {
+            recurringPayment.recurringPaymentData.deliveryData = new RecurringPaymentDeliveryData();
+            recurringPayment.recurringPaymentData.deliveryData.deliveryCity = deliveryData.deliveryCity;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryComplement = deliveryData.deliveryComplement;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryCountry = deliveryData.deliveryCountry;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryDistrict = deliveryData.deliveryDistrict;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryMail = deliveryData.deliveryMail;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryName = deliveryData.deliveryName;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryNumber = deliveryData.deliveryNumber;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryState = deliveryData.deliveryState;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryStreet = deliveryData.deliveryStreet;
+            recurringPayment.recurringPaymentData.deliveryData.deliveryZipCode = deliveryData.deliveryZipCode;
 
+            if (deliveryData.phone != null)
+            {
+                recurringPayment.recurringPaymentData.deliveryData.phone = new PhoneData();
+                recurringPayment.recurringPaymentData.deliveryData.phone.ddd = deliveryData.phone.ddd;
+                recurringPayment.recurringPaymentData.deliveryData.phone.ddi = deliveryData.phone.ddi;
+                recurringPayment.recurringPaymentData.deliveryData.phone.phone = deliveryData.phone.phone;
+                recurringPayment.recurringPaymentData.deliveryData.phone.phoneType = deliveryData.phone.phoneType;
+            }
         }
 
         public static void WithChargingData(RecurringPaymentChargingData chargingData)
         {
+            recurringPayment.recurringPaymentData.chargingData = new RecurringPaymentChargingData();
+            recurringPayment.recurringPaymentData.chargingData.birthday = chargingData.birthday;
+            recurringPayment.recurringPaymentData.chargingData.buyerMail = chargingData.buyerMail;
+            recurringPayment.recurringPaymentData.chargingData.buyerName = chargingData.buyerName;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressCity = chargingData.clientAddressCity;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressComplement = chargingData.clientAddressComplement;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressCountry = chargingData.clientAddressCountry;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressDistrict = chargingData.clientAddressDistrict;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressNumber = chargingData.clientAddressNumber;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressState = chargingData.clientAddressState;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressStreet = chargingData.clientAddressStreet;
+            recurringPayment.recurringPaymentData.chargingData.clientAddressZipcode = chargingData.clientAddressZipcode;
+            recurringPayment.recurringPaymentData.chargingData.clientCode = chargingData.clientCode;
+            recurringPayment.recurringPaymentData.chargingData.clientSex = chargingData.clientSex;
+            recurringPayment.recurringPaymentData.chargingData.clientType = chargingData.clientType;
+            recurringPayment.recurringPaymentData.chargingData.document = chargingData.document;
+            recurringPayment.recurringPaymentData.chargingData.documentTwo = chargingData.documentTwo;
+
+            if (chargingData.phone != null)
+            {
+                recurringPayment.recurringPaymentData.chargingData.phone = new PhoneData();
+                recurringPayment.recurringPaymentData.chargingData.phone.ddd = chargingData.phone.ddd;
+                recurringPayment.recurringPaymentData.chargingData.phone.ddi = chargingData.phone.ddi;
+                recurringPayment.recurringPaymentData.chargingData.phone.phone = chargingData.phone.phone;
+                recurringPayment.recurringPaymentData.chargingData.phone.phoneType = chargingData.phone.phoneType;
+            }
 
         }
 
