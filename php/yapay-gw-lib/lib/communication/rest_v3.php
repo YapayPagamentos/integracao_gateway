d3137fae39e7463993be565e7ab0a46779b384c6
Ivan Simionato <ivsimionato@gmail.com>
2018-12-13 08:40:59 -0200

    setting urls of php client

--- php/yapay-gw-lib/lib/communication/rest_v3.php
+++ php/yapay-gw-lib/lib/communication/rest_v3.php
@@ -9,48 +9,48 @@
 			$this->baseUrl = $baseUrl;
 		}
 		
-		public function transactionAuthorize($credentials, $transaction){
-			
+		public function transactionAuthorize($credential, $transaction){
+			$curl = curlInit("POST", $baseUrl."/api/v3/transacao", $transaction, $credential);
 		}
 
-		public function transactionQuery($credentials, $storeCode, $transactionNumber){
-			
+		public function transactionQuery($credential, $storeCode, $transactionNumber){
+			$curl = curlInit("GET", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 		}
 
-		public function transactionCapture($credentials, $storeCode, $transactionNumber, $value){
-			
+		public function transactionCapture($credential, $storeCode, $transactionNumber, $value){
+			$curl = curlInit("PUT", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 		}
 
-		public function transactionCancel($credentials, $storeCode, $transactionNumber, $value){
-			
+		public function transactionCancel($credential, $storeCode, $transactionNumber, $value){
+			$curl = curlInit("PUT", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 		}
 
-		public function oneclickRegister($credentials, $oneclickRegisterData){
-			
+		public function oneclickRegister($credential, $oneclickRegisterData){
+			$curl = curlInit("POST", $baseUrl."/api/v3/oneclick/", $oneclickRegisterData, $credential);
 		}
 
-		public function oneclickQuery($credentials, $token){
-			
+		public function oneclickQuery($credential, $token){
+			$curl = curlInit("GET", $baseUrl."/api/v3/oneclick/".$token, null, $credential);
 		}
 
-		public function oneclickRegisterUpdate($credentials, $token, $oneclickRegisterData){
-			
+		public function oneclickRegisterUpdate($credential, $token, $oneclickRegisterData){
+			$curl = curlInit("PUT", $baseUrl."/api/v3/oneclick/".$token."/alterar", $oneclickRegisterData, $credential);
 		}
 
-		public function oneclickAuthorize($credentials, $token, $transaction){
-			
+		public function oneclickAuthorize($credential, $token, $transaction){
+			$curl = curlInit("POST", $baseUrl."/api/v3/oneclick/".$token."/autorizar", $transaction, $credential);
 		}
 
-		public function recurringPaymentRegister($credentials, $recurringPaymentData){
-			
+		public function recurringPaymentRegister($credential, $recurringPaymentData){
+			$curl = curlInit("POST", $baseUrl."/api/v3/recorrencia", $recurringPaymentData, $credential);
 		}
 
-		public function recurringPaymentQuery($credentials, $storeCode, $recurringPaymentNumber){
-			
+		public function recurringPaymentQuery($credential, $storeCode, $recurringPaymentNumber){
+			$curl = curlInit("GET", $baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, null, $credential);
 		}
 
-		public function recurringPaymentCancel($credentials, $storeCode, $recurringPaymentNumber){
-			
+		public function recurringPaymentCancel($credential, $storeCode, $recurringPaymentNumber){
+			$curl = curlInit("PUT", $baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, $transaction, $credential);
 		}
 
 		private function curlInit($requestType, $url, $data, $credential){
