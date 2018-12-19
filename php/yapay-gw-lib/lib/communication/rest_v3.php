60b2687918c978cbf15f245344a6eb007ed1390f
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-19 17:49:38 -0200

    fixing base url variable acess in php communication

--- php/yapay-gw-lib/lib/communication/rest_v3.php
+++ php/yapay-gw-lib/lib/communication/rest_v3.php
@@ -10,77 +10,77 @@
 		}
 		
 		public function transactionAuthorize($credential, $transaction){
-			$curl = $this->curlInit("POST", $baseUrl."/api/v3/transacao", $transaction, $credential);
+			$curl = $this->curlInit("POST", $this->baseUrl."/api/v3/transacao", $transaction, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function transactionQuery($credential, $storeCode, $transactionNumber){
-			$curl = $this->curlInit("GET", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
+			$curl = $this->curlInit("GET", $this->baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function transactionCapture($credential, $storeCode, $transactionNumber, $value){
-			$curl = $this->curlInit("PUT", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
+			$curl = $this->curlInit("PUT", $this->baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function transactionCancel($credential, $storeCode, $transactionNumber, $value){
-			$curl = $this->curlInit("PUT", $baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
+			$curl = $this->curlInit("PUT", $this->baseUrl."/api/v3/transacao/".$storeCode."/".$transactionNumber, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function oneclickRegister($credential, $oneclickRegisterData){
-			$curl = $this->curlInit("POST", $baseUrl."/api/v3/oneclick/", $oneclickRegisterData, $credential);
+			$curl = $this->curlInit("POST", $this->baseUrl."/api/v3/oneclick/", $oneclickRegisterData, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function oneclickQuery($credential, $token){
-			$curl = $this->curlInit("GET", $baseUrl."/api/v3/oneclick/".$token, null, $credential);
+			$curl = $this->curlInit("GET", $this->baseUrl."/api/v3/oneclick/".$token, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function oneclickRegisterUpdate($credential, $token, $oneclickRegisterData){
-			$curl = $this->curlInit("PUT", $baseUrl."/api/v3/oneclick/".$token."/alterar", $oneclickRegisterData, $credential);
+			$curl = $this->curlInit("PUT", $this->baseUrl."/api/v3/oneclick/".$token."/alterar", $oneclickRegisterData, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function oneclickAuthorize($credential, $token, $transaction){
-			$curl = $this->curlInit("POST", $baseUrl."/api/v3/oneclick/".$token."/autorizar", $transaction, $credential);
+			$curl = $this->curlInit("POST", $this->baseUrl."/api/v3/oneclick/".$token."/autorizar", $transaction, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function recurringPaymentRegister($credential, $recurringPaymentData){
-			$curl = $this->curlInit("POST", $baseUrl."/api/v3/recorrencia", $recurringPaymentData, $credential);
+			$curl = $this->curlInit("POST", $this->baseUrl."/api/v3/recorrencia", $recurringPaymentData, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function recurringPaymentQuery($credential, $storeCode, $recurringPaymentNumber){
-			$curl = $this->curlInit("GET", $baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, null, $credential);
+			$curl = $this->curlInit("GET", $this->baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
 		}
 
 		public function recurringPaymentCancel($credential, $storeCode, $recurringPaymentNumber){
-			$curl = $this->curlInit("PUT", $baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, null, $credential);
+			$curl = $this->curlInit("PUT", $this->baseUrl."/api/v3/recorrencia/".$storeCode."/".$recurringPaymentNumber, null, $credential);
 			$response = curl_exec($curl);
 			curl_close($curl);
 			return $response;
