8b4bdf39ac502b205f87fba51e74f2833d7efd13
Ivan Simionato <ivan.simionato@yapay.com.br>
2018-12-28 15:17:04 -0200

    sending a useragent in ruby for identify lib

--- ruby/yapay_gw/lib/yapay_gw/communication/rest_v3.rb
+++ ruby/yapay_gw/lib/yapay_gw/communication/rest_v3.rb
@@ -6,7 +6,7 @@ class RestV3
     def transaction_authorize(credentials, transaction)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .post("#{BaseURL.get_url}/api/v3/transacao", :body => ActiveSupport::JSON.encode(transaction)) 
     end
 
@@ -17,7 +17,7 @@ class RestV3
     def transaction_query(credentials, store_code, transaction_number)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .get("#{BaseURL.get_url}/api/v3/transacao/#{store_code}/#{transaction_number}")
     end
 
@@ -28,7 +28,7 @@ class RestV3
     def transaction_capture(credentials, store_code, transaction_number, value)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .put("#{BaseURL.get_url}/api/v3/transacao/#{store_code}/#{transaction_number}/capturar")
     end
 
@@ -39,42 +39,42 @@ class RestV3
     def transaction_cancel(credentials, store_code, transaction_number, value)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .put("#{BaseURL.get_url}/api/v3/transacao/#{store_code}/#{transaction_number}/cancelar")
     end
 
     def oneclick_register(credentials, oneclick_register_data)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .post("#{BaseURL.get_url}/api/v3/oneclick", :body => ActiveSupport::JSON.encode(oneclick_register_data))
     end
 
     def oneclick_query(credentials, token)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .get("#{BaseURL.get_url}/api/v3/oneclick/#{token}")
     end
 
     def oneclick_register_update(credentials, token, oneclick_register_data)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .put("#{BaseURL.get_url}/api/v3/oneclick/#{token}/alterar", :body => ActiveSupport::JSON.encode(oneclick_register_data))
     end
 
     def oneclick_authorize(credentials, token, transaction)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .post("#{BaseURL.get_url}/api/v3/oneclick/#{token}/autorizar", :body => ActiveSupport::JSON.encode(oneclick_payment_data))
     end
 
     def recurring_payment_register(credentials, recurring_payment_data)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .post("#{BaseURL.get_url}/api/v3/recorrencia", :body => ActiveSupport::JSON.encode(recurring_payment_data))
     end
 
@@ -85,7 +85,7 @@ class RestV3
     def recurring_payment_query(credentials, store_code, recurring_payment_number)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .get("#{BaseURL.get_url}/api/v3/recorrencia/#{store_code}/#{recurring_payment_number}")
     end
 
@@ -96,7 +96,7 @@ class RestV3
     def recurring_payment_cancel(credentials, store_code, recurring_payment_number)
         HTTP.basic_auth(:user => credentials.user, 
                         :pass => credentials.password)
-            .headers(:content_type => "application/json")
+            .headers(:user_agent => "Gateway Ruby Client", :content_type => "application/json")
             .put("#{BaseURL.get_url}/api/v3/recorrencia/#{store_code}/#{recurring_payment_number}/cancelar")
     end
 end
